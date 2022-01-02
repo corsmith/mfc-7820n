46f9084da2e6b5510d7396cc17b9e512bb2c84b5
Corey Smith <corsmith@gmail.com>
2022-01-02 11:45:47 -0500

    clean up main.go order and whitespace

--- src/main.go
+++ src/main.go
@@ -10,10 +10,10 @@ func main() {
 	const brotherPort int = 54921
 
 	brotherIP := flag.String("a", "192.168.0.157", "IP address of the Brother scanner")
-	resolution := flag.Int("r", 300, "Resolution of the scan")
 	color := flag.String("c", "CGRAY", "Color mode of the scan (CGRAY, TEXT)")
-	name := flag.String("n", "scan.tiff", "Name of the output file")
 	rawinput := flag.String("i", "", "raw input file to parse instead of socket")
+	name := flag.String("n", "scan.tiff", "Name of the output file")
+	resolution := flag.Int("r", 300, "Resolution of the scan")
 
 	flag.Parse()
 
@@ -26,7 +26,6 @@ func main() {
 	for i, rawImage := range rawImages {
 		if i == len(rawImages)-1 {
 			SaveImage(rawImage, width, height, fmt.Sprintf("%s-%d.tiff", *name, i), *color)
-
 		} else {
 			go SaveImage(rawImage, width, height, fmt.Sprintf("%s-%d.tiff", *name, i), *color)
 		}
